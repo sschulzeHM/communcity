717d0d4628b4f31c88a5066ba8024fb93b2f9a87
Andre <andre@42ways.de>
2015-06-17 11:57:57 +0200

    gemfile.lock update

--- db/migrate/20150611084437_change_long_lat_column_types.rb
+++ db/migrate/20150611084437_change_long_lat_column_types.rb
@@ -2,14 +2,14 @@ class ChangeLongLatColumnTypes < ActiveRecord::Migration
   def up
     change_column :organisations, :longitude, :float
     change_column :organisations, :latitude, :float
-    # change_column :users, :longitude, :float
-    # change_column :users, :latitude, :float
+    change_column :users, :longitude, :float
+    change_column :users, :latitude, :float
   end
 
   def down
     change_column :organisations, :longitude, :integer
     change_column :organisations, :latitude, :integer
-    # change_column :users, :longitude, :integer
-    # change_column :users, :latitude, :integer
+    change_column :users, :longitude, :integer
+    change_column :users, :latitude, :integer
   end
 end
