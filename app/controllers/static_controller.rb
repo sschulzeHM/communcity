82c76bea2f7ed76c9543712b9999753c06109a0b
Andre <andre@42ways.de>
2015-06-25 14:59:26 +0200

    some more layout changes as suggested, large signup button etc

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def index
-    @top_projects = Project.order(:score).limit(3)
-    @top_users = User.order(:score).limit(10)
+    @top_projects = Project.order(score: :desc).limit(3)
+    @top_users = User.order(score: :desc).limit(10)
 
   end
 end
